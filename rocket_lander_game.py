03df756ee44fe0976c15026cf53f36ab655990e6
emily_IoT_2019_summer <43734279+chuyr@users.noreply.github.com>
2019-07-12 00:18:33 -0700

    python source code version 0.0.0
    python source code version 0.0.0

---
+++ rocket_lander_game.py
@@ -0,0 +1,44 @@
+# Name: Emily
+# Date: July 11th
+# Title: Rocket lander game
+
+position = 100
+velocity = 0
+acceleration = 0
+fuel = 100
+gravity = -10
+
+while position > 0:
+    print "P: " + str(position) + " V: " + str(velocity) + " F: " + str(fuel)
+    if fuel > 0:
+        thrusters = input("Set thrusters(0-20): ")
+
+        if thrusters < 0:
+            print "No thrusters (0)!"
+            thrusters = 0
+        elif thrusters >= 20:
+            print "Thrusters at max (20)!"
+            thrusters = 20
+        elif fuel < thrusters:
+            print "Out of fuel! Thrusters at " + str(fuel)
+
+        fuel -= thrusters
+        acceleration = gravity + thrusters
+        position += velocity + acceleration * 0.5
+        velocity += acceleration
+
+    else:
+        print "No fuel -- rocket is in free-fall!"
+        thrusters = 0
+        fuel -= thrusters
+        acceleration = gravity + thrusters
+        position += velocity + acceleration * 0.5
+        velocity += acceleration
+
+if velocity > -3:
+    print "Landing successful!"
+else:
+     print "Rocket crashed! Velocity was " + str(velocity) + " m/s"
+
+
+
